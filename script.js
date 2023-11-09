d1daa39912ca578caa2398a4609f60c87782306c
mibrito1 <mirian.unasp@gmail.com>
2023-11-09 16:50:31 -0300

    ajustando detalhes

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ painels.forEach(painel => {
         removeActiveClass()
         painel.classList.add('active')
     })
-
 })
 
 function removeActiveClass() {
@@ -14,20 +13,9 @@ function removeActiveClass() {
     })
 }
 
-// window.addEventListener("scroll", function () {
-//     let header = this.document.querySelector('.header')
-//     header.classList.toggle('rolagem', window.scrollY > 100)
-// })
-// function animar() {
-//     const btn = document.getElementById('btn-menu')
-//     btn.classList.toggle('ativar')
-// }
-
 const menuMobile = document.getElementById('menu-mobile')
 const btn = document.getElementById('btn-menu')
-
 menuMobile.addEventListener('click', animar)
-
 
 function animar() {
     menuMobile.classList.toggle('abrir-menu')
